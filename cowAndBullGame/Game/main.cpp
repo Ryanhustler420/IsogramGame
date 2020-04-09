9d01b26d95503d0137619e10db8cb98360d767d2
Gaurav Gupta <gouravgupta840@gmail.com>
2020-04-09 13:40:00 +0530

    constexpr usage for values which never changes

--- cowAndBullGame/Game/main.cpp
+++ cowAndBullGame/Game/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-	
+	constexpr int WORD_LENTH = 9;
+	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Can you guess the " << WORD_LENTH;
+	cout << " lette isogram I'm thinking of?\n";
 	return 0;
 }

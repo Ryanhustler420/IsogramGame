58520de206a9801946cf69c0d7f3f5c68c5f0931
Gaurav Gupta <gouravgupta840@gmail.com>
2020-04-09 15:18:35 +0530

    simplifying with functions

--- cowAndBullGame/Game/main.cpp
+++ cowAndBullGame/Game/main.cpp
@@ -5,27 +5,36 @@ using namespace std;
 
 const string EMPTY_STRING = "";
 
+// we need this empty body function signature at the top because the program needs to know which functions are gonna be the part of this program.
+void PrintIntro();
+string GetGuess();
+
+// the entry point for out application
 int main()
 {
-	// introduce the game
+	PrintIntro();
+	cout << "Your guess was: " << GetGuess();
+
+	// repeat the guess back to them
+	cout << endl;
+	return 0;
+}
+
+// get a guess from the player
+string GetGuess() {
+	cout << "Enter your guess: ";
+	string Guess = EMPTY_STRING;
+	getline(cin, Guess);
+	return Guess;
+}
+
+// introduce the game
+void PrintIntro()
+{
 	constexpr int WORD_LENTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n"; // following inconsistency meaning if using \n than use only that, btw \n doesn't flesh the output buffer
 	cout << "Can you guess the " << WORD_LENTH;
 	cout << " lette isogram I'm thinking of?\n";
 	cout << endl;
-	
-	// get a guess from the player
-	string Guess = EMPTY_STRING;
-
-	// repeat the guess back to them
-	do
-	{
-		cout << "Enter your guess: "; 
-		getline(cin, Guess);
-		cout << "Your guess was: " << Guess << endl;
-		// we gonna use getline() function to take multi word string input
-	} while (Guess != EMPTY_STRING);
-
-	cout << endl;
-	return 0;
+	return; // always do this
 }

6f890e84e09d66b1b265b307240503f582287370
Gaurav Gupta <gouravgupta840@gmail.com>
2020-04-09 15:29:38 +0530

    Iterating with For & While Loop

--- cowAndBullGame/Game/main.cpp
+++ cowAndBullGame/Game/main.cpp
@@ -13,24 +13,31 @@ string GetGuess();
 int main()
 {
 	PrintIntro();
-	cout << "Your guess was: " << GetGuess();
 
-	// repeat the guess back to them
+	// loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
+	{
+		cout << "Your guess was: " << GetGuess();
+		cout << endl;
+	}
+
 	cout << endl;
 	return 0;
 }
 
-// get a guess from the player
-string GetGuess() {
+string GetGuess() 
+{
+	// get a guess from the player
 	cout << "Enter your guess: ";
 	string Guess = EMPTY_STRING;
 	getline(cin, Guess);
 	return Guess;
 }
 
-// introduce the game
 void PrintIntro()
 {
+	// introduce the game
 	constexpr int WORD_LENTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n"; // following inconsistency meaning if using \n than use only that, btw \n doesn't flesh the output buffer
 	cout << "Can you guess the " << WORD_LENTH;

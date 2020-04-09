88b8cc66a99ef24c5426872220a93dae9aaa9f6c
Gaurav Gupta <gouravgupta840@gmail.com>
2020-04-09 14:15:15 +0530

    variables and cin for input with getline() function

--- cowAndBullGame/Game/main.cpp
+++ cowAndBullGame/Game/main.cpp
@@ -1,12 +1,31 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
+const string EMPTY_STRING = "";
+
 int main()
 {
+	// introduce the game
 	constexpr int WORD_LENTH = 9;
-	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n"; // following inconsistency meaning if using \n than use only that, btw \n doesn't flesh the output buffer
 	cout << "Can you guess the " << WORD_LENTH;
 	cout << " lette isogram I'm thinking of?\n";
+	cout << endl;
+	
+	// get a guess from the player
+	string Guess = EMPTY_STRING;
+
+	// repeat the guess back to them
+	do
+	{
+		cout << "Enter your guess: "; 
+		getline(cin, Guess);
+		cout << "Your guess was: " << Guess << endl;
+		// we gonna use getline() function to take multi word string input
+	} while (Guess != EMPTY_STRING);
+
+	cout << endl;
 	return 0;
 }
